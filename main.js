70d88b7349d952b3607e8fb4eaf0ba5af26c6d42
Ben Doggett <bdoggett@gmail.com>
2024-11-14 00:19:30 -0500

    Add code to main.js to enable posting

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-// your code here
+const postButton = document.getElementById("submit");
+
+postButton.addEventListener("click", function () {
+  const person = document.getElementById("name").value;
+  const message = document.getElementById("message").value;
+  const postsDiv = document.getElementsByClassName("posts")[0];
+
+  const postOwner = document.createElement("span");
+  postOwner.style.fontWeight = "500";
+  postOwner.innerHTML = person;
+  const postMessage = document.createElement("p");
+  postMessage.innerHTML = message;
+
+  postsDiv.appendChild(postMessage);
+  postsDiv.appendChild(postOwner);
+});
